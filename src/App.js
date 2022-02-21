51494b56682376bccadab537a71eec17bf6b9fbc
Hosnytos <hosnynaiya27@gmail.com>
2022-02-22 00:06:33 +0100

    feat: 🎸 add Genre component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import About from "./About";
 import Home from "./Home";
-import Users from "./Users";
-import User from "./User";
 
 function App() {
   return (
@@ -11,16 +8,11 @@ function App() {
       <header>
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/about">À propos</Link>
-          <Link to="/users">Users</Link>
         </nav>
       </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/users/:id" element={<User />} />
         </Routes>
       </main>
     </BrowserRouter>
---
+++ src/Genre.js
@@ -0,0 +1,11 @@
+import data from "./data.json";
+import useStyles from "./Genre.style";
+
+function Genre({ id }) {
+  const classes = useStyles();
+  const label = data.genres[id];
+  if (!label) return null;
+  return <div className={classes.root}>{label}</div>;
+}
+
+export default Genre;
