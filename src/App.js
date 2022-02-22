02c274ad7673387197e2bf1368644254db52ddcb
Hosnytos <hosnynaiya27@gmail.com>
2022-02-22 23:45:33 +0100

    feat: 🎸 add movie details page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./Home";
 import Header from "./Header";
+import Movie from "./Movie";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/movies/:id" element={<Movie />} />
         </Routes>
       </main>
     </BrowserRouter>
